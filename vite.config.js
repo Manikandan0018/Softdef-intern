117d8d1bc93fada7728a56f8a76c2a827b4678a7
manikandan <manikandan110305@gmail.com>
2025-10-25 00:19:05 +0530

    update: improved Login UI/UX and removed bg image

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [    tailwindcss(),react()],
-})
+  base: "/Softdef-intern/",
+  plugins: [tailwindcss(), react()],
+});
